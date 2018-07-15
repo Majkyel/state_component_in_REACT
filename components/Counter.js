ac6c434d2822c59280eb8f7558eafa78fd101bc2
Michał Kuliś <michalkulis1993@gmail.com>
2018-07-15 23:34:12 +0200

    Update Counter.js file

--- components/Counter.js
+++ components/Counter.js
@@ -19,25 +19,25 @@ var Counter = React.createClass({
     },
     
     getDefaultProps() {
-       console.log('getDefaultProps');
+       console.log('We use this method when we want to initialize default component props.("propTypes:")');
     },
     componentWillMount() {
-        console.log('componentWillMount');
+        console.log('This method start before render component. Prepare component to render.');
     },
     componentDidMount() {
-        console.log('componentDidMount');
+        console.log('This method start after start component. We can download some data, do some change on the component, change state component itp.');
     },
     componentWillUnmount() {
-        console.log('componentWillUnmount');
+        console.log('This method start when component is delete from DOM. Example: when we delete some Listener');
     },
     componentWillReceiveProps(nextProps) {
-        console.log('componentWillReceiveProps');
+        console.log('This method start when we initialize component in parent component and we set some properties for this child component.');
     },
     componentWillUpdate(nextProps, nextState) {
-        console.log('componentWillUpdate');
+        console.log('This method start after shouldComponentUpdate method if get "true". This method prepare component to some change. If we set new property or state');
     },
     componentDidUpdate(prevProps, prevState) {
-        console.log('componentDidUpdate');
+        console.log('THis method start after render method. In this method we can change something in props or state');
     },
     
     render: function() {
